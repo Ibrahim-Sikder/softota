21bbbfe714b09a7dd7fa5ca23cb5571fae9290f5
Your Name~Ibrahim <you@example.com>
2023-06-14 04:22:44 -0700

    completed-tour-page

---
+++ src/pages/vissa/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../../../components/Banner/Banner'
+import Countries from '../../../components/Countries/Countries'
+
+const index = () => {
+  return (
+    <>
+      <Banner></Banner>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default index
