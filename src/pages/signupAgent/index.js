c93644971e9cfba5f78df2da0d0bf639b8a10251
Your Name~Ibrahim <you@example.com>
2023-06-15 20:49:07 -0700

    signup-agent-page

---
+++ src/pages/signupAgent/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUpAgent from '../../../components/SignUpAgent/SignUpAgent'
+
+const index = () => {
+  return (
+    <div>
+      <SignUpAgent></SignUpAgent>
+    </div>
+  )
+}
+
+export default index
