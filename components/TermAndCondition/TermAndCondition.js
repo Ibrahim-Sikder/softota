78238ef6c38bf48fdfaa9be73d4f08e676e2bd5c
Your Name~Ibrahim <you@example.com>
2023-06-16 19:29:46 -0700

    term-condition-page-completed

--- components/TermAndCondition/TermAndCondition.js
+++ components/TermAndCondition/TermAndCondition.js
@@ -33,26 +33,27 @@ const TermAndCondition = () => {
           <p><strong>Use of the Services:</strong>  As a condition of using our services, you attest that you are at least 18 years of age. You intend to use our services in accordance with these terms and conditions and you will only book services from us with true and accurate information. If you book services for any other person except you, you are responsible for informing that person about our terms and conditions. </p>
           <p><strong>Deleting your ShareTrip Account:</strong> Users can delete their accounts at any time through the Profile Manage DataDelete Account in the ShareTrip app. If you submit a request to delete your account, it will be completely deleted within 72 hours.</p>
           <p><strong>Deleting your ShareTrip Account:</strong> Users can delete their accounts at any time through the Profile Manage DataDelete Account in the ShareTrip app. If you submit a request to delete your account, it will be completely deleted within 72 hours.</p>
-
-
-          <p>
-          Flight: Clients can book tickets all over the world using our state-of-the-art app and website. The client can use any local or international debit or credit card to pay for the flight after booking from the Website or App.
+     <p><strong>Flight:</strong> Clients can book tickets all over the world using our state-of-the-art app and website. The client can use any local or international debit or credit card to pay for the flight after booking from the Website or App.
 For LCC flights, users must upload a copy of their valid passport, visa & other necessary documents while issuing the tickets to avoid cancellation from the respective Airlines.
 (For example Indigo Airlines, Salam Air etc.)
-The refund amount will be disbursed after deducting the ShareTrip service fee. Please note that it takes a minimum of 10-12 bank working weeks to complete the refund process. This is not applicable for non-refundable tickets.
-* ShareTrip convenience fee is non-refundable for online purchases.
-* In case of cancellation of Bangladesh Biman Airlines flight, the refund will be processed within a minimum of 12 bank working weeks as per airlines policy.
-* For Saudia Airlines flights, the refund will be processed within a minimum of 12 bank working weeks as per airlines policy.
-* In the case of Air India half portion flight refund, the refund will be processed within a minimum of 12 bank working weeks as per airlines policy.
-* In case of changing the date of your flight, we first look into that particular airline s policy.
-* Cancellation and refund policies differ from airline to airline.
-* For cancellation by Airline, ShareTrip will not charge any additional fees. For voluntary cancellation charges are applicable and given 
-* For cancellations and refunds, we always follow the airline s refund policy. The airline solely holds the right to change any policy. ShareTrip has no control over the airline s policy.
-* For any refund/cancellation/date change-related queries kindly email us at flight@sharetrip.net.
-* If airlines increase the fare of a ticket during the booking period, a client has to pay the additional amount, else we will refund the paid amount.
-* In case of date change/re-issue of the ticket for a child who will be crossing the child age to adult (as per airline policy) in the new travel date, a new charge of the ticket will be the difference of airfare, plus a date change fee of an adult along with any other charges applicable as per airline policy.
-* International check-in counters close 1.5 hours before flight time & domestic counters close 1 hour before the flight time.
-          </p>
+The refund amount will be disbursed after deducting the ShareTrip service fee. Please note that it takes a minimum of 10-12 bank working weeks to complete the refund process. This is not applicable for non-refundable tickets.</p>
+    
+<ul>
+     <li>ShareTrip convenience fee is non-refundable for online purchases. </li>
+     <li> In case of cancellation of Bangladesh Biman Airlines flight, the refund will be processed within a minimum of 12 bank working weeks as per airlines policy.</li>
+     <li> For Saudia Airlines flights, the refund will be processed within a minimum of 12 bank working weeks as per airlines policy.</li>
+     <li> In the case of Air India half portion flight refund, the refund will be processed within a minimum of 12 bank working weeks as per airlines policy.</li>
+     <li> In case of changing the date of your flight, we first look into that particular airline s policy.</li>
+     <li> Cancellation and refund policies differ from airline to airline.</li>
+     <li> For cancellation by Airline, ShareTrip will not charge any additional fees. For voluntary cancellation charges are applicable and given </li>
+     <li> For cancellations and refunds, we always follow the airline s refund policy. The airline solely holds the right to change any policy. ShareTrip has no control over the airline s policy.</li>
+     <li>For any refund/cancellation/date change-related queries kindly email us at flight@sharetrip.net. </li>
+     <li> If airlines increase the fare of a ticket during the booking period, a client has to pay the additional amount, else we will refund the paid amount.</li>
+     <li> In case of date change/re-issue of the ticket for a child who will be crossing the child age to adult (as per airline policy) in the new travel date, a new charge of the ticket will be the difference of airfare, plus a date change fee of an adult along with any other charges applicable as per airline policy. </li>
+     <li> International check-in counters close 1.5 hours before flight time & domestic counters close 1 hour before the flight time.</li>
+
+</ul>
+
           </div>
       </div>
          
