27b927ad77bd37261661589fce4c685c029f0c7f
Your Name~Ibrahim <you@example.com>
2023-06-16 19:49:08 -0700

    privacy-page-completed

---
+++ components/Privacy/Privacy.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import style from './Privacy.module.css'
+const Privacy = () => {
+     return (
+          <div>
+            <div className={style.termCondition}>
+                <div>
+                <h2>Privacy Policy</h2>
+                <div>
+                     <p> <span>Information You Supply to Us:</span> We usually ask for basic things like your name, address, e-mail address, and phone number, and the details of anyone traveling with you, as well as your payment details. You might also choose to give us extra information about your Trip, such as when you expect to arrive.</p>
+               <p>When you use our services, we also collect information about the device you re using, like your computer or phone, such as your IP address, web browser, and language settings. Sometimes we get information from other people, or automatically collect more information about you.</p>
+                </div>
+                <p>Refund Modality: The company will refund the money exactly the way it was received. The Refund process will be the same as per the Payment modality. However, claiming refunds for any purchases made through MFS (Mobile Financial Services) e.g. Bkash, Nagad, Rocket etc will incur a service/cash-out charge which has to be borne by the customer. The charge amount will vary according to the MFS availed during the purchase.</p>
+                <p className='text-bold text-3xl'> How Does ShareTrip Make Use of Mobile Devices</p>
+                <p>We provide free mobile application which also involve the collection and processing of personal data in a similar manner to our website. However, our mobile applications offer the additional advantage of utilizing the location services present on your mobile device(s).</p>
+               <strong className='text-xl'>How Does ShareTrip Make Use of Social Media</strong>
+                <p>ShareTrip’s services may involve the integration of social media in various ways, which may result in the collection of some of your personal data or the receipt of some of your information by the social media provider.</p>
+                <strong className='text-xl'>What Log Data does ShareTrip Collect</strong>
+                <p>When you use our Service, in case of an error in the app we collect data and information on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.</p>
+              <strong>  We use the information in the following ways:</strong>
+               <p>Information You Supply to Us. We will use this information:To provide you with information and/or services that you request from us;
+               To contact you to provide the information requested.</p>
+               <strong>  How Does ShareTrip Use Personal Data Belonging to Children:</strong>              
+               <p>This service is not for use by anyone under the age of 13. We do not knowingly collect Personal Information from children under 13. If we discover that a child under 13 has provided us with personal information, we will immediately delete it from our servers. If you are a parent or guardian and you become aware that your child has provided us with personal information, please contact us immediately so that we can delete the information from our servers.</p>
+               <strong>Disclosure of Your Information:</strong>
+               <p>Any information you provide to us will either be emailed directly to us or may be stored on a secure server.</p>
+
+              <p> ShareTrip does not sell or trade upon any of the above-foregoing information without the consent of the user or customer. ShareTrip does not store any credit/debit card details and all card-related information is stored at the respective bank after the selection of the payment gateway.</p>
+               <p>The foregoing information collected from the users/customers/travelers is put to the following use:
+Customer name, address, phone number, traveler s name, and age are shared with applicable service providers like airlines, hotels, etc., for the purpose of reservation and booking the services for the customer/traveler.
+Information like phone number, email address, and billing address may be used for promotional purposes unless the customer/user has unsubscribed from such services. If you choose not to share this information, you can still visit the ShareTrip website, but you may be unable to access certain options, offers, and services.</p>
+
+<p>How Does ShareTrip Share Your Data with Third Parties:  
+ShareTrip s services involve various parties for different reasons and in different ways. The primary reason we share your data is to provide the Trip Provider with the necessary information to complete your Trip Reservation.</p>
+<p>In addition, we engage other parties to provide you with ShareTrip services, such as financial institutions, and advertisers of the ShareTrip corporate group. We may also share your data with governmental or other authorities if required by law in some instances.
+Users can request the deletion of their data through deletion of the user account from the app or by contacting us.</p>
+
+                
+
+                </div>
+               
+            </div>
+               
+          </div>
+        )
+}
+
+export default Privacy
---
+++ src/pages/privacy/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Privacy from '../../../components/Privacy/Privacy'
+
+const index = () => {
+  return (
+    <div>
+      <Privacy></Privacy>
+    </div>
+  )
+}
+
+export default index
