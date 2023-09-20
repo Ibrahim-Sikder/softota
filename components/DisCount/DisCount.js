2db6cdf1de06d5201d852132b8b07713c5bd8da4
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2023-09-20 10:20:30 +0600

    lottie-problem-solve

--- components/DisCount/DisCount.js
+++ components/DisCount/DisCount.js
@@ -1,34 +1,35 @@
 import React from "react";
 import style from "./DisCount.module.css";
-import Lottie from "react-lottie";
-import plane from "../../public/flight2.json";
-import flight from "../../public/plane.json";
+// import * as Lottie from "@lottiefiles/lottie-player";
+// import plane from "../../public/flight2.json";
+// import flight from "../../public/plane.json";
 const DisCount = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: flight,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  const defaultOptions2 = {
-    loop: true,
-    autoplay: true,
-    animationData: plane,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+  // const defaultOptions = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: flight,
+  //   rendererSettings: {
+  //     preserveAspectRatio: "xMidYMid slice",
+  //   },
+  // };
+  // const defaultOptions2 = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: plane,
+  //   rendererSettings: {
+  //     preserveAspectRatio: "xMidYMid slice",
+  //   },
+  // };
   return (
     <div className={style.discountWrap}>
-      <div className={style.discountLeft}>
+      {/* <div className={style.discountLeft}>
         <Lottie options={defaultOptions} height={450} width={450} />
       </div>
       <div>
       <Lottie options={defaultOptions2} height={450} width={450} />
 
-      </div>
+      </div> */}
+      
     </div>
   );
 };
--- components/Loader/Loader.js
+++ components/Loader/Loader.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import Lottie from 'react-lottie';
-import loading from '../../public/loading.json'
+import React, { useRef, useEffect } from "react";
+
+import loading from "../../public/loading.json";
+
 
 const Loader = () => {
-    const defaultOptions = {
-        loop: true,
-        autoplay: true, 
-        animationData: loading,
-        rendererSettings: {
-          preserveAspectRatio: 'xMidYMid slice'
-        }
-      };
-    return (
-        <div className='min-h-screen w-full flex items-center justify-center text-center'>
-             <div>
-            <Lottie options={defaultOptions}
-              height={300}
-              width={300}/>
-        </div>
-        </div>
-    );
+  // const ref = useRef();
+  // React.useEffect(() => {
+  //   import("@lottiefiles/lottie-player");
+  // });
+
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center text-center">
+      <div>
+      {/* <lottie-player
+          id="firstLottie"
+          ref={ref}
+          autoplay
+          controls
+          loop
+          mode="normal"
+          src={loading}
+          style={{ width: "300px", height: "300px" }}
+        ></lottie-player> */}
+
+
+        <h1>Loading.........!</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
