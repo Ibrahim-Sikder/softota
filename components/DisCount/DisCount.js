18d68ceec0d17072efae0d5ecdae42c25c92e858
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2023-09-20 13:55:11 +0600

    using-lottie-player-instead-of-react-lottie

--- components/DisCount/DisCount.js
+++ components/DisCount/DisCount.js
@@ -1,35 +1,39 @@
 import React from "react";
 import style from "./DisCount.module.css";
-// import * as Lottie from "@lottiefiles/lottie-player";
-// import plane from "../../public/flight2.json";
-// import flight from "../../public/plane.json";
+import { useRef, useEffect } from "react";
+
 const DisCount = () => {
-  // const defaultOptions = {
-  //   loop: true,
-  //   autoplay: true,
-  //   animationData: flight,
-  //   rendererSettings: {
-  //     preserveAspectRatio: "xMidYMid slice",
-  //   },
-  // };
-  // const defaultOptions2 = {
-  //   loop: true,
-  //   autoplay: true,
-  //   animationData: plane,
-  //   rendererSettings: {
-  //     preserveAspectRatio: "xMidYMid slice",
-  //   },
-  // };
+  const ref = useRef(null);
+  React.useEffect(() => {
+    import("@lottiefiles/lottie-player");
+  });
+
+
   return (
     <div className={style.discountWrap}>
-      {/* <div className={style.discountLeft}>
-        <Lottie options={defaultOptions} height={450} width={450} />
-      </div>
-      <div>
-      <Lottie options={defaultOptions2} height={450} width={450} />
-
-      </div> */}
-      
+        <div>
+     <lottie-player
+          id="firstLottie"
+          ref={ref}
+          autoplay
+          loop
+          mode="normal"
+          src='/flight3.json'
+          style={{ width: "450px", height: "400px" }}
+        ></lottie-player>
+     </div>
+     <div>
+     <lottie-player
+          id="firstLottie"
+          ref={ref}
+          autoplay
+          loop
+          mode="normal"
+          src='/flight2.json'
+          style={{ width: "500px", height: "500px" }}
+        ></lottie-player>
+     </div>
+   
     </div>
   );
 };
--- components/Loader/Loader.js
+++ components/Loader/Loader.js
@@ -1,30 +1,24 @@
 import React, { useRef, useEffect } from "react";
-
-import loading from "../../public/loading.json";
-
-
 const Loader = () => {
-  // const ref = useRef();
-  // React.useEffect(() => {
-  //   import("@lottiefiles/lottie-player");
-  // });
+  const animationUrl = '/loading.json';
+
+  const ref = useRef(null);
+  React.useEffect(() => {
+    import("@lottiefiles/lottie-player");
+  });
 
   return (
     <div className="min-h-screen w-full flex items-center justify-center text-center">
       <div>
-      {/* <lottie-player
+      <lottie-player
           id="firstLottie"
           ref={ref}
           autoplay
-          controls
           loop
           mode="normal"
-          src={loading}
+          src={animationUrl}
           style={{ width: "300px", height: "300px" }}
-        ></lottie-player> */}
-
-
-        <h1>Loading.........!</h1>
+        ></lottie-player>
       </div>
     </div>
   );
