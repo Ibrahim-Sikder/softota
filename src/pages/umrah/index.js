17d28855eba2e05cb6351836dfe96c8db94a6a97
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2023-09-24 12:07:08 +0600

    platinum-hajj-package-added

--- src/pages/umrah/index.js
+++ src/pages/umrah/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import UmrahLayout from '../../../components/UmrahHajj/Umrah/UmrahLayout';
 import HajjUmrahCard from '../../../components/Shared/HajjUmraCard/HajjUmrahCard';
 import umrah from "../../../public/assets/umrahOffer.jpg";
+import HajjLayout from '../../../components/UmrahHajj/Hajj/HajjLayout';
 const Umrah = () => {
     return (
-        <UmrahLayout>
-        <HajjUmrahCard
+        <HajjLayout>
+           <HajjUmrahCard
         img={umrah}
         heading="Economy Umrah Package "
         subheading=""
         title="Available 7-10-14 days Umrah Packages 2023 - 2024"
       ></HajjUmrahCard>
-        </UmrahLayout>
+        </HajjLayout>
     );
 };
 
